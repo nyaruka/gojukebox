2198cdc7803bb5d820d8149ec8f316854c2ad2e9
Nic Pottier <nicpottier@gmail.com>
2016-08-28 18:15:11 -0500

    Fix content-type for CSS files, server index.html for /artist URLs

--- golang/README.rst
+++ golang/README.rst
@@ -5,6 +5,9 @@ All run from the golang dir:
 $ export GOPATH=`pwd`
 $ export PATH=$PATH:$GOPATH/bin
 
+# build go-bindata
+$ go get -u github.com/jteeuwen/go-bindata/...
+
 # generates a bundled executable, make sure /dist is built on the angular side
 $ go generate github.com/nyaruka/gojukebox && go build github.com/nyaruka/gojukebox && gojukebox
 $ ./bin/gojukebox
--- golang/src/github.com/nyaruka/gojukebox/main.go
+++ golang/src/github.com/nyaruka/gojukebox/main.go
@@ -6,16 +6,39 @@ import (
 	"github.com/gorilla/mux"
 	"bytes"
 	"io"
+	"strings"
 )
 
 //go:generate go-bindata -prefix "../../../../../jukebox/dist/" -pkg main -o bindata.go ../../../../../jukebox/dist/...
 
 func static_handler(rw http.ResponseWriter, req *http.Request) {
   var path string = req.URL.Path
-  fmt.Println(path)
   if path == "" {
     path = "index.html"
   }
+
+  var contentType string;
+  if strings.HasSuffix(path, ".css") {
+    contentType = "text/css"
+  } else if strings.HasSuffix(path, ".png") {
+    contentType = "image/png"
+  } else {
+    contentType = "text/plain"
+  }
+
+  fmt.Println(path, " ", contentType)
+
+  if bs, err := Asset(path); err != nil {
+    rw.WriteHeader(http.StatusNotFound)
+  } else {
+    rw.Header().Add("Content-Type", contentType)
+    var reader = bytes.NewBuffer(bs)
+    io.Copy(rw, reader)
+  }
+}
+
+func index_handler(rw http.ResponseWriter, req *http.Request) {
+  var path = "index.html"
   if bs, err := Asset(path); err != nil {
     rw.WriteHeader(http.StatusNotFound)
   } else {
@@ -28,6 +51,7 @@ func main() {
 	// Here we are instantiating the gorilla/mux router
 	r := mux.NewRouter()
 
+	r.PathPrefix("/artist").Handler(http.HandlerFunc(index_handler))
 	r.PathPrefix("/").Handler(http.StripPrefix("/", http.HandlerFunc(static_handler)))
 	fmt.Println("Starting server on port 3000")
 	http.ListenAndServe(":3000", r)
